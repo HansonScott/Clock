f39e030694f15e02647b726a6227a3b90f19ec24
Scott Hanson <shanson77@aol.com>
2019-06-17 23:14:14 -0700

    got the hour notches, hour hand, and minute hand calculated.

--- Form1.cs
+++ Form1.cs
@@ -33,7 +33,7 @@ namespace Clock
 
         private FormatPicker ThisPicker = null;
 
-        private ClockType CurrentClockType = ClockType.Digital;
+        private ClockType CurrentClockType = ClockType.Analog;
         enum ClockType
         {
             Analog = 0,
@@ -123,6 +123,9 @@ namespace Clock
             float newSize = GetNewFontSize(this.label1.ClientSize);
             Font newFont = new Font(currentFont.FontFamily, newSize, currentFont.Style);
             this.label1.Font = newFont;
+
+            // this helps the analog clock redraw
+            this.Refresh();
         }
         #endregion
 
@@ -193,16 +196,123 @@ namespace Clock
 
         private void DrawClock(DateTime stamp, Graphics g)
         {
-            // draw circle
+            Point CenterPoint = new Point(this.DisplayRectangle.Width / 2, this.DisplayRectangle.Height / 2);
 
-            // draw hour notches
+            Rectangle clockRect = new Rectangle();
+            int DisplayDiameter = Math.Min(this.DisplayRectangle.Width, this.DisplayRectangle.Height);
+            int ClockDiameter = (int)(DisplayDiameter * 0.9);
+            int ClockRadius = ClockDiameter / 2;
+            double NotchRadius = ClockRadius * 0.90;
+            double HourhandRadius = ClockRadius * 0.5;
+            double MinutehandRadius = ClockRadius * 0.9;
+
+            float hourNotchThickness = (float)(DisplayDiameter * 0.02);
+            float HourHandWidth = (float)(DisplayDiameter * 0.02);
+            float minuteThickness = (float)(DisplayDiameter * 0.01);
+
+            clockRect.Height = ClockDiameter;
+            clockRect.Width = ClockDiameter;
+            clockRect.X = CenterPoint.X - ClockRadius;
+            clockRect.Y = CenterPoint.Y - ClockRadius;
+
+            // draw circle
+            Brush ClockBackBrush = new SolidBrush(Color.Black);
+            g.FillEllipse(ClockBackBrush, clockRect);
+
+            Brush ClockEdgeBrush = new SolidBrush(Color.HotPink);
+            g.DrawEllipse(new Pen(ClockEdgeBrush, 3), clockRect);
+
+            #region draw hour notches
+            for (double hourMark = 0.0; hourMark < 12.0; hourMark ++)
+            {
+                double degreesOfTime = hourMark * 30;
+
+                double unitDegreesOfTime = 90 - degreesOfTime;
+
+                double rads = (unitDegreesOfTime) / 180.0;
+
+                // then, to find the x and y, use cos/sin
+                double cr = Math.Cos(rads * Math.PI);
+
+                double sr = Math.Sin(rads * Math.PI);
+
+                // orient based on unit circle
+                if (rads > 0.5 && rads < 1.5)
+                    cr = -cr; 
+                if (rads > 1)
+                    sr = -sr; 
+
+                double startX = cr * NotchRadius;
+                double stopX = cr * ClockRadius;
+                double startY = sr * NotchRadius;
+                double stopY = sr * ClockRadius;
+
+                g.DrawLine(new Pen(ClockEdgeBrush, hourNotchThickness),
+                            (int)(CenterPoint.X + startX), (int)(CenterPoint.Y - startY),
+                            (int)(CenterPoint.X + stopX), (int)(CenterPoint.Y - stopY));
+
+            }
+            #endregion
 
             // draw labels?
 
-            // draw hour hand
+            #region draw hour hand
+            double h = stamp.Hour;
+            // NOTE: include partial hour too
+            double m = ((double)stamp.Minute / 60.0);
 
-            // draw minute hand
+            // each hour is 30 degrees
+            double d = (h + m) * 30;
 
+            // convert to rads
+            double dr = d / 180;
+
+            // adjust for unit circle starting at 3 O'clock
+            dr = 0.5 - dr;
+
+            // then, to find the x and y, use cos/sin
+            double cd = Math.Cos(dr * Math.PI);
+            if(dr > 0.5 && dr < 1.5) { cd = -cd; }
+            double XRightFromCenter = cd * HourhandRadius;
+
+            double sd = Math.Sin(dr * Math.PI);
+            if(dr > 1) { sd = -sd; }
+            double YUpFromCenter = sd * HourhandRadius;
+
+            g.DrawLine(new Pen(ClockEdgeBrush, HourHandWidth), 
+                        CenterPoint.X, CenterPoint.Y, 
+                        (int)(CenterPoint.X + XRightFromCenter), (int)(CenterPoint.Y - YUpFromCenter));
+            #endregion
+
+
+            #region draw minute hand
+            // convert minutes to degrees
+            double md = stamp.Minute * 6;
+
+            // convert to unit degrees
+            double umd = 90 - md;
+
+            // convert degrees to rads
+            double mrads = umd / 180;
+
+            // then, to find the x and y, use cos/sin
+            double crm = Math.Cos(mrads * Math.PI);
+
+            double srm = Math.Sin(mrads * Math.PI);
+
+            // orient based on unit circle
+            if (mrads > 0.5 && mrads < 1.5)
+                crm = -crm;
+            if (mrads > 1)
+                srm = -srm;
+
+            double stopXm = crm * MinutehandRadius;
+            double stopYm = srm * MinutehandRadius;
+
+            g.DrawLine(new Pen(ClockEdgeBrush, minuteThickness),
+                        (int)(CenterPoint.X), (int)(CenterPoint.Y),
+                        (int)(CenterPoint.X + stopXm), (int)(CenterPoint.Y - stopYm));
+            #endregion
         }
 
         #endregion
